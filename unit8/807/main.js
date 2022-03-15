a0385e29d0300be15566aa9324548536c1d9de6d
César Garí <59874063+cesargari16@users.noreply.github.com>
2022-03-15 09:55:19 +0100

    15-03-2022

---
+++ unit8/807/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let book = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkie",
+    publishedIn: 1937,
+    alreadyRead: true
+}
+
+let book2 = {
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkie",
+    publishedIn: 1954,
+    alreadyRead: true
+}

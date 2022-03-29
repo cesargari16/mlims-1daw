e47dd65a4b611acc4c3830683e2ab58673d42e87
César Garí <59874063+cesargari16@users.noreply.github.com>
2022-03-29 09:56:40 +0200

    29-03-2022

--- unit8/807/main.js
+++ unit8/807/main.js
@@ -1,15 +1,41 @@
 "use strict";
 
-let book = {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkie",
-    publishedIn: 1937,
-    alreadyRead: true
+    let book = {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkie",
+        publishedIn: 1937,
+        alreadyRead: false
+    };
+
+    let book2 = {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkie",
+        publishedIn: 1954,
+        alreadyRead: false
+    };
+
+    let book3 = {
+        title: "Bleach",
+        author: "Tite Kubo",
+        publishedIn: "2001",
+        alreadyRead: true
+    };
+
+
+
+let books = [book, book2, book3];
+
+
+function title() {
+    for (let i in books) {
+        let div = document.querySelector("div");
+        let p = document.createElement("p");
+        div.append(p);
+        if (books[i].alreadyRead === true)
+            p.innerHTML = `You already read "${books[i].title}" by ${books[i].author}`;
+        else
+            p.innerHTML = `You still need to read "${books[i].title}" by ${books[i].author}`;
+    }
 }
 
-let book2 = {
-    title: "The Lord of the Rings",
-    author: "J.R.R. Tolkie",
-    publishedIn: 1954,
-    alreadyRead: true
-}
+document.addEventListener('DOMContentLoaded', title);
